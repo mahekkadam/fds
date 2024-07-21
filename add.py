b598b83ee5303239da37bae67b5e34823f3adfce
mahekkadam <kadammahek28@gmail.com>
2024-07-21 23:03:28 +0530

    add.py

---
+++ add.py
@@ -0,0 +1,5 @@
+#addition of two numbers
+num1=float(input('enter the first number : '))
+num2=float(input("enter the second number :"))
+sum=num1+num2
+print("the sum of", num1 , "and" , num2 ,"is",sum)
